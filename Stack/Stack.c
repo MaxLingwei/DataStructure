881386a013489e99cc84425663d6c42287c119a5
MaxLingwei <u201213512@hust.edu.cn>
2016-01-25 21:18:17 +0800

    Stack

---
+++ Stack/Stack.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+#include"Stack.h"
+
+int CreateStack()
+{}
+
+int DestroyStack()
+{}
+
+int ClearStack()
+{}
+
+int IsEmptyStack()
+{}
+
+int PopStack()
+{}
+
+int PushStack()
+{}
+
+int GetLengthOfStack()
+{}
+
+int GetTopOfStack()
+{}
+
---
+++ Stack/Stack.h
@@ -0,0 +1,12 @@
+typedef struct tagStackNode
+{
+
+}
+int CreateStack();
+int DestroyStack();
+int ClearStack();
+int IsEmptyStack();
+int PopStack();
+int PushStack();
+int GetLengthOfStack();
+int GetTopOfStack();
