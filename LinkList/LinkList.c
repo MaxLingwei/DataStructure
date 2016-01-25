75da925df5153354be770b89682d1748d2b8e919
MaxLingwei <u201213512@hust.edu.cn>
2016-01-25 20:46:44 +0800

    link list

---
+++ LinkList/LinkList.c
@@ -0,0 +1,121 @@
+#include<stdio.h>
+#include<memory.h>
+#include<stdlib.h>
+
+#include"LinkList.h"
+
+int LinkList_InitList(IN LinkList *pstLinkList)
+{
+    pstLinkList->pstHead = NULL;
+    pstLinkList->pstTail = NULL;
+    pstLinkList->iListLen = 0;
+
+    return OK;
+}
+
+int LinkList_ClearList(IN LinkList *pstLinkList)
+{
+    LinkListNode pstListNode;
+    LinkListNode pstListNodeNext;
+    pstListNode = NULL;
+    pstListNodeNext = NULL;
+    
+    pstListNode = pstLinkList->pstHead;
+    while (pstLinkList->pstTail != pstListNode)
+    {
+        pstListNodeNext = pstListNode->pstNext;
+        free(pstListNode);
+        pstListNode =  pstListNodeNext;
+    }
+    free(pstLinkList->pstTail); 
+
+    pstLinkList->pstHead = NULL;
+    pstLinkList->pstTail = NULL;
+    pstLinkList->iListLen = 0;
+
+    return OK;
+}
+
+int LinkList_InsertNodeBefore(IN LinkList *pstLinkList,
+                              IN LinkListNode pstLinkListNode, 
+                              IN Data *pstData)
+{
+    LinkListNode pstNext = NULL;
+    LinkListNode pstNodeTemp = NULL;
+
+    if (NULL == pstLinkList->pstHead)
+    {
+        return ERROR;
+    }
+    pstNext = pstLinkList->pstHead;
+
+    while (pstLinkListNode != pstNext->pstNext)
+    {
+        pstNext = pstNext->pstNext;
+    }
+    pstNodeTemp = (LinkListNode)malloc(sizeof(struct tagLinkListNode));
+
+    memcpy(&(pstNodeTemp->stData), pstData, sizeof(Data));
+    pstNodeTemp->pstNext = pstNext->pstNext;
+    pstNext->pstNext = pstNodeTemp;
+    return OK;
+}
+
+int LinkList_IsListEmpty(LinkList *pstLinkList)
+{
+    int iRet = FALSE;
+
+    if (pstLinkList->pstTail == pstLinkList->pstHead)
+    {
+        iRet = TRUE;
+    }
+    else 
+    {
+        iRet = FALSE;
+    }
+
+    return iRet;
+}
+
+int LinkList_LocateNodePosition(IN LinkList *pstLinkList,
+                                IN int iPosition,
+                                OUT LinkListNode *ppstLinkListNode)
+{
+    int i;
+    LinkListNode pstNode = NULL;
+    LinkListNode pstNext = NULL;
+
+    if (pstLinkList->iListLen < iPosition)
+    {
+        (*ppstLinkListNode) = NULL;
+        return ERROR;
+    }
+
+    pstNode = pstLinkList->pstHead;
+    for (i = 0; i < iPosition; i++)
+    {
+        pstNode = pstNode->pstNext;    
+    }
+
+    (*ppstLinkListNode) = pstNode;
+    return OK;
+}
+
+int LinkList_DeleteNode(IN LinkList *pstLinkList,
+                        IN LinkListNode pstLinkListNode)
+{
+    LinkListNode pstNext = NULL;
+    LinkListNode pstTemp = NULL;
+    pstNext = pstLinkList->pstHead;
+
+    while(pstNext->pstNext != pstLinkListNode)
+    {
+        pstNext = pstNext->pstNext;
+    }
+    pstTemp = pstNext->pstNext;
+    pstNext->pstNext = pstTemp->pstNext;
+    free(pstTemp);
+
+    return OK;
+}
+
---
+++ LinkList/LinkList.h
@@ -0,0 +1,56 @@
+#define IN
+#define OUT 
+
+#define OK 0
+#define ERROR -1
+
+#define FALSE 0
+#define TRUE 1
+
+typedef struct tagData
+{
+    int iData;
+}Data;
+
+struct tagLinkListNode
+{
+    struct tagLinkListNode *pstNext;
+    Data stData;
+};
+
+typedef struct tagLinkListNode *LinkListNode;
+
+typedef struct tagLinkList
+{
+    LinkListNode pstHead;
+    LinkListNode pstTail;
+    int iListLen;
+}LinkList;
+
+LinkListNode _linklist_MakeNode();
+
+int _linklist_FreeNode();
+
+
+int LinkList_InitList(IN LinkList *pstLinkList);
+
+int LinkList_ClearList(IN LinkList *pstLinkList);
+
+int LinkList_InsertNodeBefore(IN LinkList *pstLinkList,
+                              IN LinkListNode pstLinkListNode,
+                              IN Data *pstData);
+
+int LinkList_DeleteNode(IN LinkList *pstLinkList,
+                        IN LinkListNode pstLinkListNode);
+
+int LinkList_SetNodeData();
+
+int LinkList_GetNodeData();
+
+int LinkList_GetListLength();
+
+int LinkList_IsListEmpty();
+
+int LinkList_LocateNodePosition(IN LinkList *pstLinkList,
+                                IN int iPosition, 
+                                OUT LinkListNode *pstLinkListNode);
