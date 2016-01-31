7cb41171d4f8329a596cadd797930f6e77040c20
MaxLingwei <u201213512@hust.edu.cn>
2016-01-31 21:45:32 +0800

    add cycle linked list

--- LinkList/clist.c
+++ LinkList/clist.c
@@ -36,4 +36,57 @@ int clist_destroy(CList *list)
 
 int clist_ins_next(CList *list, CListElmt *element, const void *data)
 {
+    CListElmt *new_element =NULL;
+
+    new_element = (CListElmt *)malloc(sizeof(CListElmt));
+    if (NULL == new_element)
+    {
+        return ERROR;
+    }
+
+    new_element->data = (void *)data;
+    if (EMPTY == list->size)
+    {
+        list->head = new_element;
+        new_element->next = new_element;
+    }
+    else
+    {
+        new_element->next = element->next;
+        element->next = new_element->next;
+    }
+    list->size++;
+    return OK;
+}
+
+int clist_rem_next(CList *list, CListElmt *element, void **data)
+{
+    CListElmt *old_element = NULL;
+
+    if (EMPTY == clist_size(list))
+    {
+        return ERROR;
+    }
+
+    *data = element->next;
+
+    if (element->next == element)
+    {
+        old_element = element;
+        list->head = NULL;
+    }
+    else
+    {
+        old_element = element->next;
+        element->next = element->next->next;
+
+        if (old_element == list_head(list))
+        {
+            list->head = old_element->next;
+        }
+    }
+
+    free(old_element);
+    list->size--;
+    return OK;
 }
